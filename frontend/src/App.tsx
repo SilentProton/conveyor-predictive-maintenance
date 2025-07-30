cc54f707e3193a6c9a2076e2cd4560624bdc9250
SilentProton <silentproton.25@gmail.com>
2025-07-30 20:24:37 +0530

    feat: initialize frontend with base components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import { FileUpload } from './components/ui/FileUpload';
 
+function App() {
   return (
     <>
-      
+      <div className="flex w-screen h-screen ">
+        {/* Left Panel */}
+        <div className="flex-1 text-white flex items-center justify-center h-screen">
+          <FileUpload />
+        </div>
+
+        {/* Right Panel */}
+        <div className="flex-1 text-white flex items-center justify-center">
+          <h1 className="text-2xl font-bold">Output Panel</h1>
+        </div>
+      </div>
     </>
   );
 }
